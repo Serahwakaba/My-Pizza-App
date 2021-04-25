1078b6f7bc3c0706a4eb573b3cbd400754f23ecb
Serah wakaba <serahwakaba9@gmail.com>
2021-04-25 21:09:56 +0300

    my switch commit

--- js/scripts.js
+++ js/scripts.js
@@ -40,66 +40,66 @@ alert("Confirmed, Your order shall be delivered at" + " " +location)
 var deliveryCost = parseInt(0);
 };
 
-// var pizzaPrice = "";
-// switch(pizzaSize){
-// case "1":
-// pizzaPrice = 790;
-// break;
-// case "2":
-// pizzaPrice = 690;
-// break;
-// case "3":
-// pizzaPrice = 300;
-// break;
-// default:
-// console.log("No price");
-// }
+var pizzaPrice = "";
+switch(pizzaSize){
+case "1":
+pizzaPrice = 790;
+break;
+case "2":
+pizzaPrice = 690;
+break;
+case "3":
+pizzaPrice = 300;
+break;
+default:
+console.log("No price");
+}
 
-// var crustPrice = "";
-// switch(crust){
-// case "1":
-// crustPrice =100;
-// break;
-// case "2":
-// crustPrice = 150;
-// break;
-// case "3":
-// crustPrice = 200;
-// break;
-// default:
-// console.log("No price");
-// }
+var crustPrice = "";
+switch(crust){
+case "1":
+crustPrice =100;
+break;
+case "2":
+crustPrice = 150;
+break;
+case "3":
+crustPrice = 200;
+break;
+default:
+console.log("No price");
+}
 
-// var toppingPrice = "";
-// switch(toppings){
-// case "1":
-// toppingPrice = 0;
-// break;
-// case "2":
-// toppingPrice = 99;
-// break;
-// case "3":
-// toppingPrice = 149;
-// break;
-// case "4":
-// toppingPrice = 199;
-// break;
-// default:
-// console.log("No price");
-// };
+var toppingPrice = "";
+switch(toppings){
+case "1":
+toppingPrice = 0;
+break;
+case "2":
+toppingPrice = 99;
+break;
+case "3":
+toppingPrice = 149;
+break;
+case "4":
+toppingPrice = 199;
+break;
+default:
+console.log("No price");
+};
 
-// var deliveryPrice = "";
-// switch (delivery) {
-// case "1":
-// deliveryPrice = 0;
-// break;
-// case "2":
-// deliveryPrice = 100;
-// break;
-// default:
-// console.log("No price");
+var deliveryPrice = "";
+switch (delivery) {
+case "1":
+deliveryPrice = 0;
+break;
+case "2":
+deliveryPrice = 100;
+break;
+default:
+console.log("No price");
 
-// };
+};
 // var subTotalPrice = parseInt(pizzaPrice) + parseInt(crustPrice) + parseInt(toppingPrice)
 // console.log(subTotalPrice)
 // let checkOutTotal =0;
